54030c475d260de58e774e9a454b2cc6484ba872
ademola <ademola@quickcheck.ng>
2021-10-20 14:43:08 +0100

    codebase cleanup

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
-from .views import ContactPageView, HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('about/', AboutPageView.as_view(), name="about"),
-    path('contact/', ContactPageView.as_view(), name="contact"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,16 +2,3 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-class HomePageView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "main/home.html", {})
-
-
-class AboutPageView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "main/about.html", {})
-
-
-class ContactPageView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "main/contact.html", {})
